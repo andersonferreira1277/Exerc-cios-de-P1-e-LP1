233b055225a6964c56ee2657fec2d4db19ecac80
Anderson Ferreira Câmara <andersonferreira1277@gmail.com>
2017-11-17 16:06:51 -0300

    Exercícios de PyQt5

---
+++ pyQt5/09.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.initMyWindow()
+    def initMyWindow(self):
+        e1 = QLineEdit()
+        e1.setValidator(QIntValidator())
+        e1.setMaxLength(4)
+        e1.setAlignment(Qt.AlignRight)
+        e1.setFont(QFont("Arial",20))
+
+        e2=QLineEdit()
+        e2.setValidator(QDoubleValidator(0.99,99.99,2))
+
+        flo=QFormLayout()
+        flo.addRow("integer validator", e1)
+        flo.addRow("Double validator",e2)
+
+        e3=QLineEdit()
+        e3.setInputMask('+99_9999_999999')
+        flo.addRow("Input Mask",e3)
+
+        e4=QLineEdit()
+        e4.textChanged.connect(self.textchanged)
+        flo.addRow("Text changed",e4)
+
+        e5=QLineEdit()
+        e5.setEchoMode(QLineEdit.Password)
+        flo.addRow("Password",e5)
+
+        e6=QLineEdit("Hello Python")
+        e6.setReadOnly(True)
+        flo.addRow("Read Only",e6)
+
+        e5.editingFinished.connect(self.enterPress)
+
+        self.setLayout(flo)
+        self.setWindowTitle("PyQt")
+        self.show()
+
+    def textchanged(text):
+        print ("contents of text box: "+text)
+    def enterPress():
+        print ("edited")
+
+app = QApplication(sys.argv)
+frame = MyWindow()
+app.exec_()
