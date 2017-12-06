a8a37e2c016785f711599e3c2b5f1eefb3491370
Anderson Ferreira Câmara <andersonferreira1277@gmail.com>
2017-12-06 10:52:06 -0300

    Começando projeto de gerar declaração.

---
+++ GeradorDeDocumentos/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+andersonferreira1277@gmail.com
+"""
+from PyQt5.QtWidgets import QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.initMainWindow()
+    def initMainWindow(self):
+        pass
