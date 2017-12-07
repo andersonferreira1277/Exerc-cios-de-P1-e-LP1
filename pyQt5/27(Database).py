969caf5f24d23630d23276989ef74c1b9900d4ab
Anderson Ferreira Câmara <andersonferreira1277@gmail.com>
2017-12-07 08:49:00 -0300

    QSqlDataBase e QMessageBox.

---
+++ pyQt5/27(Database).py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+andersonferreira1277@gmail.com
+"""
+import sys
+from PyQt5 import QtSql
+from PyQt5.QtWidgets import *
+
+def createDB():
+    db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+    db.setDatabaseName('sports.db')
+    if not db.open():
+        QMessageBox.critical(None, "Cannot open database","Unable to establish a database connection.\n"+
+                                                                           "This example needs SQLite support."+
+                                                                           " Please read the Qt SQL driver documentation"+
+                                                                           " for information how to build it.\n\nClick Cancel to exit.",
+                                                                           QMessageBox.Cancel)
+        return False
+    query = QtSql.QSqlQuery()
+    query.exec_("create table sportsmen(id int primary key, "+
+                "firstname varchar(20), lastname varchar(20))")
+    query.exec_("insert into sportsmen values(101, 'Roger', 'Federer')")
+    query.exec_("insert into sportsmen values(102, 'Christiano', 'Ronaldo')")
+    query.exec_("insert into sportsmen values(103, 'Ussain', 'Bolt')")
+    query.exec_("insert into sportsmen values(104, 'Sachin', 'Tendulkar')")
+    query.exec_("insert into sportsmen values(105, 'Saina', 'Nehwal')")
+    QMessageBox.information(None, "Informação", "Conectado ao banco de dados", QMessageBox.Ok)
+    return True
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    createDB()
---
+++ pyQt5/QMessageBox.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+andersonferreira1277@gmail.com
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+
+def message():
+    QMessageBox.information(None, 'PyQt5 message', "Do you like PyQt5?", QMessageBox.Yes)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    message()
