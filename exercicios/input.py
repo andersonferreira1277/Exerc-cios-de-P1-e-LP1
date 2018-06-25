c7be7e059ccb46244f6bb29eca616fcdba4494f4
Anderson Ferreira Câmara <andersonferreira1277@gmail.com>
2018-06-25 20:07:17 -0300

    Mais alguns exercícios.

---
+++ exercicios/input.py
@@ -0,0 +1,6 @@
+a = int(input("Digite um valor"))
+b = int(input("Digite outro valor"))
+c = a+b
+c = str(c)
+print("A soma é: "+c)
+
---
+++ exercicios/variaveis.py
@@ -0,0 +1,15 @@
+a = 150
+b = 250
+c = a+b
+print(c)
+d = a-b
+print(d)
+e = a*b
+print(e)
+f = a/b
+print(f)
+
+
+
+
+
