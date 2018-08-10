8772b0a1cc8812bd044e56a6908c0d4cf2369286
Anderson Ferreira Câmara <andersonferreira1277@gmail.com>
2018-08-10 18:02:13 -0300

    Melhorando o teste de conexão.

--- exercicios/testeDeConexaoComInternet.py
+++ exercicios/testeDeConexaoComInternet.py
@@ -1,20 +1,30 @@
-#-*- coding: utf-8 -*-
-import socket, datetime
-confiaveis = ['www.google.com', 'www.uol.com.br', 'www.globo.com.br']
+# -*- coding: utf-8 -*-
+import socket, datetime, time, csv
+confiaveis = ['www.google.com', 'www.uol.com.br', 'www.globo.com']
 
 
 def check_host(confiaveis):
-	for host in confiaveis:
-		a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		a.settimeout(.5)
-		try:
-			b = a.connect_ex((host, 80))
-			if b == 0:
-				print("Conectou a: "+host)
-				print("Hora: "+datetime.datetime.now().strftime("%T"))
-		except:
-			print("Não conectou a: "+host)
-	a.close()
+
+    while True:
+
+        for host in confiaveis:
+            a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            a.settimeout(.5)
+            try:
+                b = a.connect_ex((host, 80))
+                if b == 0:
+                    s = "Conectou a: " + host
+                    s = s + " - Data e hora: " + datetime.datetime.now().strftime("%Y-%m-%d %T")
+            except:
+                s = "Não conectou a: " + host
+                s = s + " - Data e hora: " + datetime.datetime.now().strftime("%Y-%m-%d %T")
+            with open("relatorioInternet.txt", 'a') as arquivo:
+                arquivo.write(s)
+                arquivo.write("\n")
+            a.close()
+        time.sleep(60*30)
+
 
 if __name__ == '__main__':
-	check_host(confiaveis)
+
+    check_host(confiaveis)
