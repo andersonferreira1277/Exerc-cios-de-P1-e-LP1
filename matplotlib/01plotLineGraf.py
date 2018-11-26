3716dac362cdaf249ed526d13b53884e520ec501
Anderson Ferreira Câmara <andersonferreira1277@gmail.com>
2018-11-26 19:05:35 -0300

    Grafico linear com legenda.

--- matplotlib/01plotLineGraf.py
+++ matplotlib/01plotLineGraf.py
@@ -6,8 +6,8 @@ andersonferreira1277@gmail.com
 
 import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3, 4])
+plt.plot(['01/2018', '02/2018', '03/2018', '04/2018'], [500.15, 700.5, 850.56, 450.56] )
 plt.ylabel('some numbers') # legenda do eixo y
 plt.xlabel("values") # legenda do eixo x
-plt.title("Anderson")
+plt.title("Histórico de saldo")
 plt.show()
